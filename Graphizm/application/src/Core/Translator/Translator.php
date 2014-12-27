996dbadd473334c05ebb708720061616643ef77c
Aurélien <aurelien.muller13@gmail.com>
2014-12-27 14:55:38 +0100

    Tanslator module initialized

---
+++ Graphizm/application/src/Core/Translator/Translator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Translation function.
+ *
+ * @param string $value
+ *   Key or value.
+ *
+ * @return string
+ *   Standard translation of input, or input itself if no translation found.
+ */
+function t($value) {
+    // @TODO : do stuff to translate text.
+    return $value;
+}
