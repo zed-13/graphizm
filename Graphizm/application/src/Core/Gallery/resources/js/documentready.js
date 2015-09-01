b4af7d525f2aa56638df2ca73cacf858883557f5
Aurélien <aurelien.muller13@gmail.com>
2015-09-01 02:02:43 +0200

    Special case: no gallery selected

--- Graphizm/application/src/Core/Gallery/resources/js/documentready.js
+++ Graphizm/application/src/Core/Gallery/resources/js/documentready.js
@@ -5,11 +5,11 @@
      * Show or hide elements.
      * @param string identifier.
      */
-    var checker = function(o){
+    var checker = function(o) {
         var id_base = "#" + $(o).attr("class");
         $(id_base + "_check").attr("checked","checked");
-        $(id_base).show("slow");
-        $(id_base + "_").show("slow");
+        $(id_base).show();
+        $(id_base + "_").show();
     };
 
     /**
@@ -17,15 +17,27 @@
      * @param string identifier.
      */
     var toto = function(o) {
-        $("#" + o).toggle("slow");
-        $("#" + o + "_").toggle("slow");
+        $("#" + o).toggle();
+        $("#" + o + "_").toggle();
+        checkNonEmpty();
+    };
+
+    /**
+     * Checks if at least a gallery is selected.
+     */
+    var checkNonEmpty = function() {
+        if($("input:checkbox:checked").length === 0) {
+            $(".btn-style-grpzm").last().parent().append($("<div class='no-gallery js-no-gallery'> Sélectionnez une galerie.</div>"));
+        } else {
+            $(".js-no-gallery").remove();
+        }
     };
 
     /**
      * Document ready stuff.
      */
-    $(document).ready(function(){
-        $("input:checkbox").each(function(){
+    $(document).ready(function() {
+        $("input:checkbox").each(function() {
             if($(this).is(":checked")) {
             } else {
                 toto($(this).attr("value"));
@@ -34,6 +46,11 @@
         $(".checkcheckcheck-yourbooty").each(function() {
             $(this).click(function() {
                 checker($(this));
+                var a = $(this).parent().find("input:checkbox");
+                if(!a.is(":checked")) {
+                    a.prop('checked', true);
+                    toto(a.attr("value"));
+                }
             });
         });
         $(".input-menu_checkbox").each(function() {
